9ab2e9e6aa3e1c9a284ff23769a7d93d44ea3f30
Billy Ponto <bpgponto@gmail.com>
2023-07-03 18:11:11 +0800

    starter template of redux toolkit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Counter from "./features/counter/Counter"
+
+function App() {
+	return (
+		<>
+			<h1 className="text-5xl font-bold text-rose-800">Counter with React Redux</h1>
+			<Counter />
+		</>
+	)
+}
+
+export default App
